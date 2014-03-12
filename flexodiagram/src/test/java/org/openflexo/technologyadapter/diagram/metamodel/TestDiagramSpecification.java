b18b3acefc043b11077c11922b6a6139e0ed34af
Vincent <vincent@openflexo.org>
2014-03-12 14:53:04 +0100

    LOW / Create tests for DiagramSpecification management

--- flexodiagram/src/test/java/org/openflexo/technologyadapter/diagram/metamodel/TestDiagramSpecification.java
+++ flexodiagram/src/test/java/org/openflexo/technologyadapter/diagram/metamodel/TestDiagramSpecification.java
@@ -1,14 +1,23 @@
 package org.openflexo.technologyadapter.diagram.metamodel;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.awt.Color;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openflexo.fge.geom.FGEPoint;
+import org.openflexo.fge.shapes.ShapeSpecification.ShapeType;
 import org.openflexo.foundation.OpenflexoTestCase;
 import org.openflexo.model.ModelEntity;
 import org.openflexo.model.exceptions.ModelDefinitionException;
+import org.openflexo.technologyadapter.diagram.model.Diagram;
+import org.openflexo.technologyadapter.diagram.model.DiagramConnector;
+import org.openflexo.technologyadapter.diagram.model.DiagramFactory;
+import org.openflexo.technologyadapter.diagram.model.DiagramShape;
 import org.openflexo.technologyadapter.diagram.model.DiagramSpecificationFactory;
 import org.openflexo.test.OrderedRunner;
 import org.openflexo.test.TestOrder;
@@ -22,6 +31,10 @@ import org.openflexo.test.TestOrder;
 @RunWith(OrderedRunner.class)
 public class TestDiagramSpecification extends OpenflexoTestCase{
 
+	public static DiagramSpecification diagramSpecification;
+	public static DiagramPalette diagramPalette;
+	public static DiagramSpecificationFactory factory;
+	
 	/**
 	 * Test the diagram specification factory
 	 */
@@ -29,8 +42,10 @@ public class TestDiagramSpecification extends OpenflexoTestCase{
 	@TestOrder(1)
 	public void testDiagramSpecificationFactory() {
 
+		log("testDiagramSpecificationFactory()");
+		
 		try {
-			DiagramSpecificationFactory factory = new DiagramSpecificationFactory();
+			factory = new DiagramSpecificationFactory();
 
 			ModelEntity<DiagramSpecification> diagramSpecificationEntity = factory.getModelContext().getModelEntity(DiagramSpecification.class);
 			ModelEntity<DiagramPalette> paletteEntity = factory.getModelContext().getModelEntity(DiagramPalette.class);
@@ -52,12 +67,11 @@ public class TestDiagramSpecification extends OpenflexoTestCase{
 	@Test
 	@TestOrder(2)
 	public void testInstanciateDiagramSpecfication() throws Exception {
-
-		DiagramSpecificationFactory factory = new DiagramSpecificationFactory();
-
-		DiagramSpecification diagramSpecification = factory.makeNewDiagramSpecification();
+		
+		log("testInstanciateDiagramSpecfication()");
+		
+		diagramSpecification = factory.makeNewDiagramSpecification();
 		assertTrue(diagramSpecification instanceof DiagramSpecification);
-
 	}
 	
 	/**
@@ -66,13 +80,46 @@ public class TestDiagramSpecification extends OpenflexoTestCase{
 	@Test
 	@TestOrder(3)
 	public void testInstanciatePalette() throws Exception {
-
+		
+		log("testInstanciatePalette()");
+		
 		DiagramPaletteFactory factory = new DiagramPaletteFactory();
-
 		DiagramPalette diagramPalette = factory.makeNewDiagramPalette();
 		assertNotNull(diagramPalette);
 		assertTrue(diagramPalette instanceof DiagramPalette);
+	}
 	
+	/**
+	 * Test fill the Diagram Specification
+	 */
+	@Test
+	@TestOrder(4)
+	public void testFillDiagramSpecification() throws Exception {
+		
+		log("testFillDiagramSpecification()");
+		
+		// Create an example diagram
+		DiagramFactory factory = new DiagramFactory();
+		Diagram diagram = factory.newInstance(Diagram.class);
+		assertTrue(diagram instanceof Diagram);
+		DiagramShape shape1 = factory.makeNewShape("Shape1", ShapeType.RECTANGLE, new FGEPoint(100, 100), diagram);
+		shape1.getGraphicalRepresentation().setForeground(factory.makeForegroundStyle(Color.RED));
+		shape1.getGraphicalRepresentation().setBackground(factory.makeColoredBackground(Color.BLUE));
+		assertTrue(shape1 instanceof DiagramShape);
+		DiagramShape shape2 = factory.makeNewShape("Shape2", ShapeType.RECTANGLE, new FGEPoint(200, 100), diagram);
+		shape2.getGraphicalRepresentation().setForeground(factory.makeForegroundStyle(Color.BLUE));
+		shape2.getGraphicalRepresentation().setBackground(factory.makeColoredBackground(Color.WHITE));
+		assertTrue(shape2 instanceof DiagramShape);
+		DiagramConnector connector1 = factory.makeNewConnector("Connector", shape1, shape2, diagram);
+		assertTrue(connector1 instanceof DiagramConnector);
+		
+		// Add the palette and the example diagram
+		diagramSpecification.addToExampleDiagrams(diagram);
+		diagramSpecification.addToPalettes(diagramPalette);
+		
+		assertTrue(diagramSpecification.getExampleDiagrams().size()>0);
+		assertTrue(diagramSpecification.getPalettes().size()>0);
+		
 	}
 
 }
--- flexodiagram/src/test/java/org/openflexo/technologyadapter/diagram/metamodel/TestDiagramSpecificationResource.java
+++ flexodiagram/src/test/java/org/openflexo/technologyadapter/diagram/metamodel/TestDiagramSpecificationResource.java
@@ -2,16 +2,25 @@ package org.openflexo.technologyadapter.diagram.metamodel;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.awt.Color;
 import java.io.File;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openflexo.fge.geom.FGEPoint;
+import org.openflexo.fge.shapes.ShapeSpecification.ShapeType;
 import org.openflexo.foundation.OpenflexoTestCase;
 import org.openflexo.foundation.TestFlexoServiceManager;
 import org.openflexo.foundation.resource.FlexoResourceCenter;
 import org.openflexo.foundation.resource.SaveResourceException;
+import org.openflexo.model.exceptions.ModelDefinitionException;
 import org.openflexo.technologyadapter.diagram.DiagramTechnologyAdapter;
+import org.openflexo.technologyadapter.diagram.model.Diagram;
+import org.openflexo.technologyadapter.diagram.model.DiagramConnector;
+import org.openflexo.technologyadapter.diagram.model.DiagramFactory;
+import org.openflexo.technologyadapter.diagram.model.DiagramShape;
 import org.openflexo.technologyadapter.diagram.rm.DiagramPaletteResource;
 import org.openflexo.technologyadapter.diagram.rm.DiagramPaletteResourceImpl;
 import org.openflexo.technologyadapter.diagram.rm.DiagramSpecificationRepository;
@@ -34,33 +43,56 @@ public class TestDiagramSpecificationResource extends OpenflexoTestCase{
 	private final String diagramSpecificationURI = "http://myDiagramSpecification";
 	private final String paletteName = "myDiagramSpecificationPalette";
 	
+	public static DiagramSpecificationResource diagramSpecificationResource;
+	public static DiagramPaletteResource paletteResource;
+	public static FlexoResourceCenter<?> resourceCenter;
+	public static DiagramTechnologyAdapter technologicalAdapter;
+	public static TestFlexoServiceManager applicationContext;
+	public static DiagramSpecificationRepository repository;
+	
+	/**
+	 * Initialize
+	 */
+	@Test
+	@TestOrder(1)
+	public void testInitialize() {
+
+		log("testInitialize()");
+
+		applicationContext = new TestFlexoServiceManager(new FileResource(new File(resourcesFolder).getAbsolutePath()));
+		technologicalAdapter = applicationContext.getTechnologyAdapterService().getTechnologyAdapter(
+				DiagramTechnologyAdapter.class);
+		resourceCenter = applicationContext.getResourceCenterService().getResourceCenters().get(0);
+		repository = resourceCenter.getRepository(DiagramSpecificationRepository.class, technologicalAdapter);
+		
+		assertNotNull(applicationContext);
+		assertNotNull(technologicalAdapter);
+		assertNotNull(resourceCenter);
+		assertNotNull(repository);
+	}
+
+	
+	
 	/**
 	 * Test Create diagram specification resource
 	 */
 	@Test
-	@TestOrder(1)
+	@TestOrder(2)
 	public void testCreateDiagramSpecificationResource() {
+		
+		log("testCreateDiagramSpecificationResource()");
 
 		try {
-			TestFlexoServiceManager applicationContext = new TestFlexoServiceManager(new FileResource(
-					new File(resourcesFolder).getAbsolutePath()));
-			DiagramTechnologyAdapter technologicalAdapter = applicationContext.getTechnologyAdapterService().getTechnologyAdapter(
-					DiagramTechnologyAdapter.class);
-	
-			FlexoResourceCenter<?> resourceCenter = applicationContext.getResourceCenterService().getResourceCenters().get(0);
-		
-			DiagramSpecificationRepository repository = resourceCenter.getRepository(DiagramSpecificationRepository.class, technologicalAdapter);
 			File diagramRep = new File(repository.getDirectory() + "/"+diagramFileName);
 			File diagramFile = new File(diagramRep+ "/"+diagramFileName+DiagramSpecificationResource.DIAGRAM_SPECIFICATION_SUFFIX);
-			DiagramSpecificationResource resource = DiagramSpecificationResourceImpl.makeDiagramSpecificationResource(diagramSpecificationURI, diagramRep, 
+			diagramSpecificationResource = DiagramSpecificationResourceImpl.makeDiagramSpecificationResource(diagramSpecificationURI, diagramRep, 
 					diagramFile, applicationContext);
 			
-			resource.save(null);
-			assertTrue(resource.getFile().exists());
+			diagramSpecificationResource.save(null);
+			assertTrue(diagramSpecificationResource.getFile().exists());
 			
 		} catch (SaveResourceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			fail(e.getMessage());
 		}
 		
 	}
@@ -69,41 +101,67 @@ public class TestDiagramSpecificationResource extends OpenflexoTestCase{
 	 * Test Load diagram specification resource
 	 */
 	@Test
-	@TestOrder(2)
+	@TestOrder(3)
 	public void testLoadDiagramSpecificationResource() {
-
-		TestFlexoServiceManager applicationContext = new TestFlexoServiceManager(new FileResource(
-				new File(resourcesFolder).getAbsolutePath()));
-	
-		DiagramSpecificationResource resource = DiagramSpecificationResourceImpl.retrieveDiagramSpecificationResource(new File(resourcesFolder+"/"+diagramFileName), applicationContext);
-			
-		assertNotNull(resource);
 		
+		log("testLoadDiagramSpecificationResource()");
+		
+		DiagramSpecificationResource resource = DiagramSpecificationResourceImpl.retrieveDiagramSpecificationResource(new File(resourcesFolder+"/"+diagramFileName), applicationContext);
+		assertNotNull(resource);
 	}
 	
 	/**
 	 * Test Create Palette resource
 	 */
 	@Test
-	@TestOrder(3)
-	public void testCreatePaletteResource() {
-
+	@TestOrder(4)
+	public void testUpdateDiagramSpecificationResourceWithPaletteResource() {
+		
+		log("testUpdateDiagramSpecificationResourceWithPaletteResource()");
+		
 		try {
-			TestFlexoServiceManager applicationContext = new TestFlexoServiceManager(new FileResource(
-					new File(resourcesFolder).getAbsolutePath()));
-			DiagramSpecificationResource resource = DiagramSpecificationResourceImpl.retrieveDiagramSpecificationResource(new File(resourcesFolder+"/"+diagramFileName), applicationContext);
-			DiagramPaletteResource palette = DiagramPaletteResourceImpl.makeDiagramPaletteResource(resource, paletteName, applicationContext);
-			
-			palette.save(null);
-			assertTrue(palette.getFile().exists());
-			resource.save(null);
-			assertTrue(resource.getDiagramPaletteResources().contains(palette));
+			paletteResource = DiagramPaletteResourceImpl.makeDiagramPaletteResource(diagramSpecificationResource, paletteName, applicationContext);
+			paletteResource.save(null);
+			assertTrue(paletteResource.getFile().exists());
+			diagramSpecificationResource.save(null);
+			assertTrue(diagramSpecificationResource.getDiagramPaletteResources().contains(paletteResource));
 		} catch (SaveResourceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			fail(e.getMessage());
 		}
 		
 	}
+	
+	/**
+	 * Test update diagram specification resource with diagram resource
+	 */
+	@Test
+	@TestOrder(5)
+	public void testUpdateDiagramSpecificationResourceWithDiagramResource() {
+		try {
+			// Create an example diagram
+			DiagramFactory factory = new DiagramFactory();
+			Diagram diagram = factory.newInstance(Diagram.class);
+			DiagramShape shape1 = factory.makeNewShape("Shape1", ShapeType.RECTANGLE, new FGEPoint(100, 100), diagram);
+			shape1.getGraphicalRepresentation().setForeground(factory.makeForegroundStyle(Color.RED));
+			shape1.getGraphicalRepresentation().setBackground(factory.makeColoredBackground(Color.BLUE));
+			DiagramShape shape2 = factory.makeNewShape("Shape2", ShapeType.RECTANGLE, new FGEPoint(200, 100), diagram);
+			shape2.getGraphicalRepresentation().setForeground(factory.makeForegroundStyle(Color.BLUE));
+			shape2.getGraphicalRepresentation().setBackground(factory.makeColoredBackground(Color.WHITE));
+			DiagramConnector connector1 = factory.makeNewConnector("Connector", shape1, shape2, diagram);
+			
+			// Add the palette and the example diagram
+			diagramSpecificationResource.getDiagramSpecification().addToExampleDiagrams(diagram);
+			diagramSpecificationResource.getDiagramSpecification().addToPalettes(paletteResource.getDiagramPalette());
+			diagramSpecificationResource.save(null);
+			
+		} catch (SaveResourceException e) {
+			fail(e.getMessage());
+		} catch (ModelDefinitionException e) {
+			fail(e.getMessage());
+		}
+		
+	}
+	
 	
 	
 }
