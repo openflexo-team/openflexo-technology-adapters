9dfcceab08882844a7098cb993910fc6ab89c2b2
Vincent <vincent@openflexo.org>
2014-04-23 17:16:23 +0200

    LOW / Remove old content

--- flexodiagram/src/main/java/org/openflexo/technologyadapter/diagram/model/action/ExportDiagramToImageAction.java
+++ flexodiagram/src/main/java/org/openflexo/technologyadapter/diagram/model/action/ExportDiagramToImageAction.java
@@ -87,60 +87,6 @@ public class ExportDiagramToImageAction extends FlexoGUIAction<ExportDiagramToIm
 
 	private File dest;
 
-	/*public boolean saveAsImage() {
-		dest = null;
-		JFileChooser chooser = new JFileChooser() {
-			@Override
-			public void approveSelection() {
-				File f = getSelectedFile();
-				if (f.exists() && getDialogType() == SAVE_DIALOG) {
-					int result = JOptionPane.showConfirmDialog(this, "The file exists, overwrite?", "Existing file",
-							JOptionPane.YES_NO_CANCEL_OPTION);
-					switch (result) {
-					case JOptionPane.YES_OPTION:
-						super.approveSelection();
-						return;
-					case JOptionPane.NO_OPTION:
-						return;
-					case JOptionPane.CLOSED_OPTION:
-						return;
-					case JOptionPane.CANCEL_OPTION:
-						cancelSelection();
-						return;
-					}
-				}
-				if (!f.exists() && getDialogType() == SAVE_DIALOG) {
-					super.approveSelection();
-					return;
-				}
-			}
-		};
-		chooser.setDialogType(JFileChooser.SAVE_DIALOG);
-		chooser.setDialogTitle(FlexoLocalization.localizedForKey("save_as_image", chooser));
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG Images", "png");
-		chooser.setFileFilter(filter);
-		int returnVal = chooser.showSaveDialog(null);
-		if (returnVal == JFileChooser.CANCEL_OPTION) {
-			return false;
-		}
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			if (!chooser.getSelectedFile().getName().toLowerCase().endsWith(".png")) {
-				dest = new File(chooser.getSelectedFile().getAbsolutePath() + ".png");
-			} else {
-				dest = chooser.getSelectedFile();
-				// dest = new File(chooser.getSelectedFile().getAbsolutePath());
-			}
-		}
-		if (dest == null) {
-			return false;
-		}
-		if (saveScreenshot() != null) {
-			return true;
-		} else {
-			return false;
-		}
-	}*/
-	
 	private ImageType imageType;
 
 	public boolean saveAsImage() {
