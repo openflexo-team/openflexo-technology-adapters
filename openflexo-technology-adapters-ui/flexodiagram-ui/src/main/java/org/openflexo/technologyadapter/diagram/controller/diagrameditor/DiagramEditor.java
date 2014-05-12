e1362adcee96d3d51ce417ce29f68144cfc125a5
Vincent <vincent@openflexo.org>
2014-05-12 10:39:34 +0200

    LOW / TA-18

--- openflexo-technology-adapters-ui/flexodiagram-ui/src/main/java/org/openflexo/technologyadapter/diagram/controller/diagrameditor/DiagramEditor.java
+++ openflexo-technology-adapters-ui/flexodiagram-ui/src/main/java/org/openflexo/technologyadapter/diagram/controller/diagrameditor/DiagramEditor.java
@@ -115,7 +115,8 @@ public abstract class DiagramEditor extends SelectionManagingDianaEditor<Diagram
 
 			toolsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 			toolsPanel.add(toolSelector.getComponent());
-			toolsPanel.add(stylesWidget.getComponent());
+			// This panel is in its current state a duplication of the right side panel
+			// toolsPanel.add(stylesWidget.getComponent());
 			toolsPanel.add(layoutWidget.getComponent());
 
 			commonPaletteModel = makeCommonPalette();
