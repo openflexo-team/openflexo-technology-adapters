a7b62781ab49212fee99ab33b092008f08634975
Vincent <vincent@openflexo.org>
2014-10-20 18:25:01 +0200

    MEDIUM / [FlexoIODelegate] Create a FlexoIODelegate for CVS Resources

--- csvconnector/src/main/java/org/openflexo/technologyadapter/csv/rm/CSVModelResource.java
+++ csvconnector/src/main/java/org/openflexo/technologyadapter/csv/rm/CSVModelResource.java
@@ -1,6 +1,5 @@
 package org.openflexo.technologyadapter.csv.rm;
 
-import org.openflexo.foundation.resource.FlexoFileResource;
 import org.openflexo.foundation.technologyadapter.FlexoModelResource;
 import org.openflexo.model.annotations.Getter;
 import org.openflexo.model.annotations.ImplementationClass;
@@ -13,8 +12,7 @@ import org.openflexo.technologyadapter.csv.model.CSVModel;
 
 @ModelEntity
 @ImplementationClass(CSVModelResourceImpl.class)
-public abstract interface CSVModelResource extends FlexoFileResource<CSVModel>,
-		FlexoModelResource<CSVModel, CSVMetaModel, CSVTechnologyAdapter> {
+public abstract interface CSVModelResource extends FlexoModelResource<CSVModel, CSVMetaModel, CSVTechnologyAdapter> {
 	public static final String TECHNOLOGY_CONTEXT_MANAGER = "technologyContextManager";
 
 	@Getter(value = "technologyContextManager", ignoreType = true)
