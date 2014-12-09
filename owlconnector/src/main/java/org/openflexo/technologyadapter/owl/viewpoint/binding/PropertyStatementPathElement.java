69210d884ad1e9c3ec3303b6fd0043671c7a2913
Sylvain Guérin <sylvain@openflexo.org>
2014-12-09 13:55:52 +0100

    MEDIUM / Fixed missing getBindingValue() computations

--- owlconnector/src/main/java/org/openflexo/technologyadapter/owl/viewpoint/binding/PropertyStatementPathElement.java
+++ owlconnector/src/main/java/org/openflexo/technologyadapter/owl/viewpoint/binding/PropertyStatementPathElement.java
@@ -124,6 +124,9 @@ public abstract class PropertyStatementPathElement extends SimplePathElement {
 
 		@Override
 		public Object getBindingValue(Object target, BindingEvaluationContext context) throws TypeMismatchException, NullReferenceException {
+			if (target instanceof OWLIndividual) {
+				return ((OWLIndividual) target).getPropertyValue(getProperty());
+			}
 			logger.warning("Please implement me, target=" + target + " context=" + context);
 			return null;
 		}
@@ -148,6 +151,9 @@ public abstract class PropertyStatementPathElement extends SimplePathElement {
 
 		@Override
 		public Object getBindingValue(Object target, BindingEvaluationContext context) throws TypeMismatchException, NullReferenceException {
+			if (target instanceof OWLIndividual) {
+				return ((OWLIndividual) target).getPropertyValue(getProperty());
+			}
 			logger.warning("Please implement me, target=" + target + " context=" + context);
 			return null;
 		}
