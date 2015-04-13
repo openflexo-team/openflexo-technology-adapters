2bb6fa7821a83bae98b9607dc95136fad59a70ce
Sylvain Guérin <sylvain@openflexo.org>
2015-04-13 10:25:09 +0200

    LOW / Attempt to set size of DIANA inspectors panel (does not work)

--- openflexo-technology-adapters-ui/flexodiagram-ui/src/main/java/org/openflexo/technologyadapter/diagram/controller/DiagramTechnologyAdapterController.java
+++ openflexo-technology-adapters-ui/flexodiagram-ui/src/main/java/org/openflexo/technologyadapter/diagram/controller/DiagramTechnologyAdapterController.java
@@ -38,6 +38,7 @@
 
 package org.openflexo.technologyadapter.diagram.controller;
 
+import java.awt.Dimension;
 import java.util.Collections;
 import java.util.List;
 
@@ -155,6 +156,8 @@ public class DiagramTechnologyAdapterController extends TechnologyAdapterControl
 		scaleSelector = swingToolFactory.makeDianaScaleSelector(null);
 		dialogInspectors = swingToolFactory.makeDianaDialogInspectors();
 		inspectors = swingToolFactory.makeDianaInspectors();
+		//inspectors.getPanelGroup().setPreferredSize(new Dimension(800, 800));
+		//inspectors.getPanelGroup().setMinimumSize(new Dimension(500, 500));
 
 		dialogInspectors.getForegroundStyleInspector().setLocation(1000, 100);
 		dialogInspectors.getTextStyleInspector().setLocation(1000, 300);
