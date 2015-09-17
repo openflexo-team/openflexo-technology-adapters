06f65429a3408dc78455e5f0edf3ec56db328dfe
Sylvain Guérin <sylvain@openflexo.org>
2015-09-17 09:08:51 +0200

    MEDIUM / Removed getURI() method

--- emfconnector/src/main/java/org/openflexo/technologyadapter/emf/rm/EMFModelResourceImpl.java
+++ emfconnector/src/main/java/org/openflexo/technologyadapter/emf/rm/EMFModelResourceImpl.java
@@ -68,7 +68,7 @@ import org.openflexo.toolbox.IProgress;
  * @author sguerin
  * 
  */
-public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel>implements EMFModelResource {
+public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel> implements EMFModelResource {
 
 	private static final Logger logger = Logger.getLogger(EMFModelResourceImpl.class.getPackage().getName());
 
@@ -88,8 +88,8 @@ public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel>im
 	public static EMFModelResource makeEMFModelResource(String modelURI, File modelFile, EMFMetaModelResource emfMetaModelResource,
 			EMFTechnologyContextManager technologyContextManager, FlexoResourceCenter<?> resourceCenter) {
 		try {
-			ModelFactory factory = new ModelFactory(
-					ModelContextLibrary.getCompoundModelContext(FileFlexoIODelegate.class, EMFModelResource.class));
+			ModelFactory factory = new ModelFactory(ModelContextLibrary.getCompoundModelContext(FileFlexoIODelegate.class,
+					EMFModelResource.class));
 			EMFModelResourceImpl returned = (EMFModelResourceImpl) factory.newInstance(EMFModelResource.class);
 			returned.setTechnologyAdapter(technologyContextManager.getTechnologyAdapter());
 			returned.setTechnologyContextManager(technologyContextManager);
@@ -118,15 +118,14 @@ public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel>im
 	/**
 	 * URI here is the full path to the file
 	 */
-	@Override
+	/*@Override
 	public String getURI() {
 		// TODO FIX THIS When refactoring with clean IoDelegate support
 		return ((FileFlexoIODelegate) getFlexoIODelegate()).getFile().toURI().toString();
-	}
+	}*/
 
 	/**
-	 * Instanciates a new {@link OWLOntologyResource} asserting we are about to built a resource matching an existing file in the file
-	 * system<br>
+	 * Instanciates a new {@link EMFModelResource} asserting we are about to built a resource matching an existing file in the file system<br>
 	 * This method should not be used to explicitely build a new ontology
 	 * 
 	 * @param owlFile
@@ -136,8 +135,8 @@ public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel>im
 	public static EMFModelResource retrieveEMFModelResource(File modelFile, EMFMetaModelResource emfMetaModelResource,
 			EMFTechnologyContextManager technologyContextManager, FlexoResourceCenter<?> resourceCenter) {
 		try {
-			ModelFactory factory = new ModelFactory(
-					ModelContextLibrary.getCompoundModelContext(FileFlexoIODelegate.class, EMFModelResource.class));
+			ModelFactory factory = new ModelFactory(ModelContextLibrary.getCompoundModelContext(FileFlexoIODelegate.class,
+					EMFModelResource.class));
 			EMFModelResourceImpl returned = (EMFModelResourceImpl) factory.newInstance(EMFModelResource.class);
 			returned.setTechnologyAdapter(technologyContextManager.getTechnologyAdapter());
 			returned.setTechnologyContextManager(technologyContextManager);
@@ -268,8 +267,7 @@ public abstract class EMFModelResourceImpl extends FlexoResourceImpl<EMFModel>im
 			if (mmResource == null) {
 				logger.warning("EMFModel has no meta-model !!!");
 				return null;
-			}
-			else {
+			} else {
 				if (!mmResource.isLoaded()) {
 					try {
 						mmResource.loadResourceData(null);
