29e0f2773d7b8d7a159413107c349f91adf21070
Sylvain Guérin <sylvain@openflexo.org>
2016-04-14 15:59:16 +0200

    MEDIUM / Fixed GINA-20

--- openflexo-technology-adapters-ui/freeplaneconnector-ui/src/main/java/org/openflexo/technologyadapter/freeplane/listeners/FreeplaneNodeMouseListener.java
+++ openflexo-technology-adapters-ui/freeplaneconnector-ui/src/main/java/org/openflexo/technologyadapter/freeplane/listeners/FreeplaneNodeMouseListener.java
@@ -44,53 +44,53 @@ import java.awt.event.MouseEvent;
 import org.freeplane.features.mode.Controller;
 import org.freeplane.view.swing.map.NodeView;
 import org.freeplane.view.swing.ui.mindmapmode.MNodeMotionListener;
+import org.openflexo.gina.swing.view.SwingViewFactory.SwingFIBMouseEvent;
 import org.openflexo.view.controller.FlexoController;
 
 public class FreeplaneNodeMouseListener extends MNodeMotionListener {
 
-    private final FlexoController controller;
+	private final FlexoController controller;
 
-    public FreeplaneNodeMouseListener(final FlexoController controller) {
-        super();
-        this.controller = controller;
-    }
+	public FreeplaneNodeMouseListener(final FlexoController controller) {
+		super();
+		this.controller = controller;
+	}
 
-    @Override
-    public void showPopupMenu(final MouseEvent e) {
-        this.triggerSelection(e);
-        if (!e.isPopupTrigger()) {
-            return;
-        }
-        if (this.controller.getSelectionManager().getSelection().size() == 1) {
-            this.controller.objectWasRightClicked(this.controller.getSelectionManager().getSelection().get(0), e);
-        }
-    }
+	@Override
+	public void showPopupMenu(final MouseEvent e) {
+		this.triggerSelection(e);
+		if (!e.isPopupTrigger()) {
+			return;
+		}
+		if (this.controller.getSelectionManager().getSelection().size() == 1) {
+			this.controller.objectWasRightClicked(this.controller.getSelectionManager().getSelection().get(0), new SwingFIBMouseEvent(e));
+		}
+	}
 
-    /**
-     * See inheritance of freeplane, but just a copy paste with a control
-     * statement removed, to force selection.
-     * 
-     * @param e
-     *            MouseEvent
-     */
-    private void triggerSelection(final MouseEvent e) {
+	/**
+	 * See inheritance of freeplane, but just a copy paste with a control statement removed, to force selection.
+	 * 
+	 * @param e
+	 *            MouseEvent
+	 */
+	private void triggerSelection(final MouseEvent e) {
 		if (!isInside(e)) {
 			return;
 		}
-        this.nodeSelector.stopTimerForDelayedSelection();
+		this.nodeSelector.stopTimerForDelayedSelection();
 
-        final NodeView nodeV = this.nodeSelector.getRelatedNodeView(e);
+		final NodeView nodeV = this.nodeSelector.getRelatedNodeView(e);
 		Controller.getCurrentController().getSelection().selectAsTheOnlyOneSelected(nodeV.getModel());
-        Controller.getCurrentModeController().getMapController().onSelect(nodeV.getModel());
-    }
+		Controller.getCurrentModeController().getMapController().onSelect(nodeV.getModel());
+	}
 
-    /**
+	/**
 	 * Wasn't public on node selector ==> CopyPaste
 	 * 
 	 * @param e
 	 * @return
 	 */
-    private static boolean isInside(final MouseEvent e) {
-        return new Rectangle(0, 0, e.getComponent().getWidth(), e.getComponent().getHeight()).contains(e.getPoint());
-    }
+	private static boolean isInside(final MouseEvent e) {
+		return new Rectangle(0, 0, e.getComponent().getWidth(), e.getComponent().getHeight()).contains(e.getPoint());
+	}
 }
