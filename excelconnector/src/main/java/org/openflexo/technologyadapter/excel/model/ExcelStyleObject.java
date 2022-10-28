d80e944696b68df23ef4be642eec69f5c3713c50
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2022-10-28 08:49:49 +0200

    MEDIUM / pptx and xlsx moved to their own repositories

--- excelconnector/src/main/java/org/openflexo/technologyadapter/excel/model/ExcelStyleObject.java
+++
@@ -1,9 +0,0 @@
-package org.openflexo.technologyadapter.excel.model;
-
-import org.openflexo.technologyadapter.excel.model.ExcelStyleManager.CellStyleFeature;
-
-public interface ExcelStyleObject {
-	
-	public void setStyle(CellStyleFeature cellStyle, Object value);
-	
-}
